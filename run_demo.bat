8d84934f8ae1601e0c55c5a438bf423ecccba385
BenTartaglia <bdt5263@psu.edu>
2026-03-03 19:30:30 -0500

    Update run_demo.bat

--- run_demo.bat
+++ run_demo.bat
@@ -8,11 +8,15 @@ python -m venv venv
 echo Activating virtual environment...
 call venv\Scripts\activate
 
+echo Upgrading pip...
+python -m pip install --upgrade pip
+
 echo Installing dependencies...
 python -m pip install -r requirements.txt
 
 echo Running betting model with Kelly Criterion...
 python main.py -xgb -kc
 
+echo.
 echo Demo complete.
 pause
